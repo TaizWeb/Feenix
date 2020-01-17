d8c514621282439663d23901da126d6f695d112c
taiz <tyler.walker.student@gmail.com>
2020-01-17 02:39:46 -0500

    .

--- server.py
+++ server.py
@@ -4,21 +4,9 @@ from flask import Flask
 from flask import render_template
 from flask import request
 app = Flask(__name__)
-# TODO cancel quotes in sql
+
 def string_escape(string):
 	return string.replace("\"", "\"\"").replace("\'", "\'\'")
-
-# TODO
-# v0.2.0 will allow users with accounts to talk to each other on an ugly html page
-# v0.3.0 will work more on the database and getting it to store messages effectively
-# v0.4.0 will have different channels (thus more tables)
-# v0.5.0 will have server configuration via .json files
-# v0.6.0 will include assignable rolls and heirarchy. Prepare for a bunch of if statements.
-# v0.7.0 will include some detection stuff like "is typing..." and offline statuses
-# v0.8.0 will include markdown support and link detection, along with emojis
-# v0.9.0 will include image uploading and profile pics
-# v1.0.0 will have a complete front-end redesign and any last-minute features
-# End result of 1.0 is a finished product, one that would allow people to run the software on a vps and allow for a functional chatroom. 2.0 will probably include some federation stuff.
 
 @app.route('/')
 def hello():
